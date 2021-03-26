3c2424c0034aff4658383c9be3d4e511cd34081b
Niloo <niloofargst@gmail.com>
2021-03-26 10:17:42 +0100

    fixing

---
+++ ex03/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function* myGenerator() {
+    yield*[1, 2, 3, 4, 5];
+    yield*"Arena";
+    yield* arguments;
+}
+
+const iterator = myGenerator(6, 7, 8);
+const generatorArray = [];
+
+
+for (let i = 0; i < 13; i++) {
+    generatorArray.push(iterator.next().value);
+}
+
+console.log(generatorArray);
+// Only change code above this line
+module.exports = { generatorArray, myGenerator };
